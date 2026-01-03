1a476f2e0230966b7e76f49dc952bb9058081eac
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-03 23:48:53 +0600

    install mongoose and and other npm package and index.js code uppate

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import mongoose, { mongo } from "mongoose";
+import { DB_NAME } from "./constants";
+
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("error:", error);
+      throw error;
+    });
+  } catch (error) {
+    console.error("Error :", error);
+    throw error;
+  }
+})();
