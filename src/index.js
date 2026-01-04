28da053be0e0fa113db4a81c5b61545d36ce57b2
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-04 12:34:49 +0600

    express

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 
@@ -5,4 +6,16 @@ import dotenv from "dotenv";
 dotenv.config({ path: "./env" });
 
 //  connection data base
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port : ${process.env.PORT}`);
+    });
+    app.on("error", (error) => {
+      console.log("Error:", error);
+      throw error;
+    });
+  })
+  .catch((error) => {
+    console.log("mongoDB connection field", error);
+  });
